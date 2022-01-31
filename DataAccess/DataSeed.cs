eb4f252654d3fdc0c43f979513d7aa5d696e6f61
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-01-31 12:02:18 +0200

    Added Data Access Layer.

---
+++ DataAccess/DataSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models.classes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataSeed
+    {
+        public static async Task SeedDataAsync(JWTDbContext context, UserManager<User> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<User>
+                                {
+                                    new User
+                                        {
+                                            FirstName = "FirstName1",
+                                            LastName = "LastName1",
+                                            UserName = "TestUserFirst",
+                                            Email = "testuserfirst@test.com"
+                                        },
+
+                                    new User
+                                        {
+                                            FirstName = "FirstName2",
+                                            LastName = "LastName2",
+                                            UserName = "TestUserSecond",
+                                            Email = "testusersecond@test.com"
+                                        }
+                                };
+
+                foreach(var user in users)
+                {
+                    await userManager.CreateAsync(user, "qazwsX123@");
+                }
+            }
+        }
+    }
+}
---
+++ DataAccess/JWTDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Models.classes;
+
+namespace DataAccess
+{
+    public class JWTDbContext : IdentityDbContext<User, Role, int>
+    {
+        //public JWTDbContext(DbContextOptions<JWTDbContext> options) : base(options)
+        //{
+        //    Database.EnsureCreated();
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=.\sqlexpress;Initial Catalog=JWT;Integrated Security=True;");
+        }
+    }
+}
