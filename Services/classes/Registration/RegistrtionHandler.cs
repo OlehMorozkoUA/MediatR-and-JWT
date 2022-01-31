169d0e0d3905ec3c17de269f290b4b6b0bf3c28b
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-01-31 22:56:12 +0200

    Added RegistrationHandler.

---
+++ Services/classes/Registration/RegistrtionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Models.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.classes.Registration
+{
+    public class RegistrtionHandler
+    {
+        private readonly UserManager<User> _userManager; 
+    }
+}
